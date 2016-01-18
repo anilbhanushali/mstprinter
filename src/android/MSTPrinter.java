869cd31ca0d72715ceab48cecd7fb21342913a1f
Toshan Verma <v.toshan@better-life.co.in>
2016-01-18 14:11:57 +0530

    Update MSTPrinter.java file

--- src/android/MSTPrinter.java
+++ src/android/MSTPrinter.java
@@ -7,10 +7,9 @@ public class MSTPrinter extends CordovaPlugin {
 	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException
-	{
-			 
+	{	 
+		return true;
 	};
 	
-	return true;
 		
 }
