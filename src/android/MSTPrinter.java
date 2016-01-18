759576775ecde350f22fe874712c1f346fa87b32
Toshan Verma <v.toshan@better-life.co.in>
2016-01-18 19:59:42 +0530

    Update MSTPrinter.java file

--- src/android/MSTPrinter.java
+++ src/android/MSTPrinter.java
@@ -19,30 +19,37 @@ public class MSTPrinter extends CordovaPlugin implements DeviceCallBacks {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        DeviceBluetoothCommunication bluetoothCommunication;
-        bluetoothCommunication = new DeviceBluetoothCommunication();
+        try{
+            DeviceBluetoothCommunication bluetoothCommunication;
+            bluetoothCommunication = new DeviceBluetoothCommunication();
 
-        BluetoothAdapter mBluetoothAdapter;
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            BluetoothAdapter mBluetoothAdapter;
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        if(ACTION_CONNECT_PRINTER.equals(action)){
+            if(ACTION_CONNECT_PRINTER.equals(action)){
 
-            //call connect printer method
-            JSONObject arg_object = args.getJSONObject(0);
-            BluetoothDevice macid = mBluetoothAdapter.getRemoteDevice(arg_object.getString("macaddress"));
-            //arg_object.getString("macaddress");
-            bluetoothCommunication.StartConnection(macid,this);
-            return true;
+                //call connect printer method
+                JSONObject arg_object = args.getJSONObject(0);
+                BluetoothDevice macid = mBluetoothAdapter.getRemoteDevice(arg_object.getString("macaddress"));
+                bluetoothCommunication.StartConnection(macid,this);
+                callbackContext.success();
+                return true;
+            }
+            else if (ACTION_PRINT_TEXT.equals(action)) {
+
+                //call print text method
+                String name = "Toshan Verma";
+                bluetoothCommunication.SendData(name.getBytes());
+                callbackContext.success();
+                return true;
+            }
+
+            return false;
         }
-        else if (ACTION_PRINT_TEXT.equals(action)) {
-
-            //call print text method
-            String name = "Toshan Verma";
-            bluetoothCommunication.SendData(name.getBytes());
-            return true;
+        catch (Exception e){
+            callbackContext.error(e.getMessage());
+            return false;
         }
-
-        return false;
     }
 
     @Override
